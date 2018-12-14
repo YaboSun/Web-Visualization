bd99f311ae5ee04aba72e8d569cc81342a55ae4c
Yabosun <yabosun@163.com>
2018-12-14 21:28:41 +0800

    initial commit

---
+++ src/main/java/ybsun/top/spark/WebVisualizationApplication.java
@@ -0,0 +1,14 @@
+package ybsun.top.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebVisualizationApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/ybsun/top/spark/WebVisualizationApplicationTests.java
@@ -0,0 +1,17 @@
+package ybsun.top.spark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebVisualizationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
